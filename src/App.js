7250249d8cbbd17ddba3a49e4a26e290546ecf70
Ashk02H <ashwinikawale079@gmail.com>
2023-04-25 11:23:04 +0530

    demo

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
 
-      hello ashwini
+      hello ashwini kawale
       {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="`logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
